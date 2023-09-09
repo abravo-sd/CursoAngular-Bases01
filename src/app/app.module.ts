acd1de6e8c80e966e7971d2e70c8ce82c038a201
Andres Bravo <abravo.sd@gmail.com>
2023-09-09 18:03:03 -0500

    Terminando Modulo 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { ContadorComponent } from './Contador/contador.component';
 import { HeroesModule } from './Heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
-    
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
